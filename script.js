3dd1d59f6f595699066870ad8d640deacc880b58
Paweł D <you@example.com>
2024-01-04 14:38:48 +0100

    renderBooks function

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const submitButton = document.querySelector('button[type="submit"]');
-
+const bookContainer = document.querySelector('.books-container');
 
 const myLibrary = [
     {
         title:'The Hobbit',
         author:'J.R.R. Tolkien',
         pages:'295',
-        read: false,
+        read: 'not',
         info: function() {
             console.log(this.title,'by',this.author,',', this.pages,'pages,', this.read)
             // console.log;(this.title)
         },
     }
 ];
-
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -24,8 +23,7 @@ function Book(title, author, pages, read) {
         console.log(this.title,'by',this.author,',', this.pages,'pages,', this.read)
         // console.log;(this.title)
     };
-  }
-
+  };
 
 function addBookToLibrary(){
     console.log('taking data...')
@@ -37,30 +35,48 @@ function addBookToLibrary(){
     const newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook);
     console.log(title, 'should be in library!')
-}
-
-
+};
 
 submitButton.addEventListener('click', function() {
     addBookToLibrary()
 });
 
-//   na promtpach!
-  /* function addBookToLibrary() {
-    console.log('addBookToLibrary-working...');
-    const title = prompt('title');
-    const author = prompt('author');
-    const pages = prompt('pages');
-    let read; 
-        while (true) {
-            read = prompt('Czy przeczytane? (tak/nie)').toLowerCase();
-            if (read === 'tak' || read === 'nie') {
-                break;
-            }
-            alert('Proszę podać odpowiedź "tak" lub "nie".');
-        }
-        console.log(title);
+function renderBooks(){
+    bookContainer.innerHTML = '';
 
-    }  */
-  
+    for (var i=0; i < myLibrary.length; i++){
+        var book = myLibrary[i];
+
+        var bookTitle = document.createElement('p');
+        bookTitle.classList.add('book-title');
+        bookTitle.textContent = book.title;
+        bookContainer.appendChild(bookTitle);
+
+        var bookAuthor = document.createElement('p');
+        bookAuthor.classList.add('book-title');
+        bookAuthor.textContent = book.author;
+        bookContainer.appendChild(bookAuthor);
+
+        var bookPages = document.createElement('p');
+        bookPages.classList.add('book-title');
+        bookPages.textContent = book.pages;
+        bookContainer.appendChild(bookPages);
+
+        var bookRead = document.createElement('p');
+        bookRead.classList.add('book-title');
+        bookRead.textContent = readOrNo(book.read)
+        bookContainer.appendChild(bookRead);
+
+    }
+};
+
+function readOrNo(readValue){
+    if (readValue === 'yes') {
+        return 'Read';
+    } else {
+        return 'Not read';
+    }
+}
+
+renderBooks()
 
